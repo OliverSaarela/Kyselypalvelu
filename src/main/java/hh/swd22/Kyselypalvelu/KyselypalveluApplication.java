1a45d28f44fecfa5aec7590eb7a6a38710467f5c
OliverSaarela <43965623+OliverSaarela@users.noreply.github.com>
2019-10-24 08:25:56 +0300

    Alku tehty

---
+++ src/main/java/hh/swd22/Kyselypalvelu/KyselypalveluApplication.java
@@ -0,0 +1,13 @@
+package hh.swd22.Kyselypalvelu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyselypalveluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyselypalveluApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hh/swd22/Kyselypalvelu/KyselypalveluApplicationTests.java
@@ -0,0 +1,13 @@
+package hh.swd22.Kyselypalvelu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyselypalveluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
