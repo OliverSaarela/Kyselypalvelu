df92a1a51584105a003d4c39a83ddf1779961cb3
OliverSaarela <43965623+OliverSaarela@users.noreply.github.com>
2019-10-25 16:30:07 +0300

    Tietokanta tehty

--- src/main/java/hh/swd22/Kyselypalvelu/domain/AnswerRepository.java
+++ src/main/java/hh/swd22/Kyselypalvelu/domain/AnswerRepository.java
@@ -1,5 +1,10 @@
 package hh.swd22.Kyselypalvelu.domain;
 
-public class AnswerRepository {
+import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
+
+public interface AnswerRepository extends CrudRepository<Answer, Long>{
+
+	List<Answer> findByTxtAnswer(String txtAnswer);
 }
