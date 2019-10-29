5fb5cd06bf7e6ad6915d5897a25794bf570d67a1
Jaakko Kammonen <jaakko.kammonen@hotmail.com>
2019-10-29 13:42:40 +0200

    Tehty Controllori metodeita lisää

--- src/main/java/hh/swd22/Kyselypalvelu/web/QuestionController.java
+++ src/main/java/hh/swd22/Kyselypalvelu/web/QuestionController.java
@@ -3,13 +3,17 @@ package hh.swd22.Kyselypalvelu.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
+import hh.swd22.Kyselypalvelu.domain.Answer;
 import hh.swd22.Kyselypalvelu.domain.AnswerRepository;
 import hh.swd22.Kyselypalvelu.domain.Form;
 import hh.swd22.Kyselypalvelu.domain.FormRepository;
+import hh.swd22.Kyselypalvelu.domain.Question;
 import hh.swd22.Kyselypalvelu.domain.QuestionRepository;
 
 public class QuestionController {
@@ -23,9 +27,36 @@ public class QuestionController {
 	@Autowired
 	private AnswerRepository aRepo;
 	
-	@RequestMapping (value="/forms", method =RequestMethod.GET)
+	
+	
+	@RequestMapping (value="/forms", method =RequestMethod.GET) // Haetaan kaikki listat
 	public @ResponseBody List <Form> FormlistREST(){
 		return (List <Form>) fRepo.findAll();
+	}
+	
+	@RequestMapping(value = "/saveforms", method = RequestMethod.POST) // Tallennetaan formi
+	public @ResponseBody Form SaveFormREST(@RequestBody Form form) {
+	
+		 return fRepo.save(form);
+		
+	}
+	
+	
+	
+	
+	
+	@RequestMapping (value="/questions", method =RequestMethod.GET) // Haetaan kaikki kys
+	public @ResponseBody List <Question> QuestionlistREST(){
+		return (List <Question>) qRepo.findAll();
+	}
+	
+	
+	
+	
+	
+	@RequestMapping (value="/answers", method =RequestMethod.GET) // Haetaan kaikki vastaukset
+	public @ResponseBody List <Answer> AnswerslistREST(){
+		return (List <Answer>) aRepo.findAll();
 	}
 	
 	
