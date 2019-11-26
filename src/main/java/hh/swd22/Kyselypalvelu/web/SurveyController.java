e9788cd1eec2c0f313b59fe594ffb80f2679477c
Jaakko Kammonen <jaakko.kammonen@hotmail.com>
2019-11-26 14:42:56 +0200

    Uus fixi taas

--- src/main/java/hh/swd22/Kyselypalvelu/web/SurveyController.java
+++ src/main/java/hh/swd22/Kyselypalvelu/web/SurveyController.java
@@ -1,5 +1,8 @@
 package hh.swd22.Kyselypalvelu.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,7 +11,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import hh.swd22.Kyselypalvelu.domain.Answer;
+import hh.swd22.Kyselypalvelu.domain.AnswerRepository;
 import hh.swd22.Kyselypalvelu.domain.QuestionRepository;
 import hh.swd22.Kyselypalvelu.domain.Survey;
 import hh.swd22.Kyselypalvelu.domain.SurveyRepository;
@@ -22,7 +29,39 @@ public class SurveyController {
 
 	@Autowired
 	private QuestionRepository qRepo;
+	
+	@Autowired
+	private AnswerRepository aRepo;
 
+	// Kaikki REST-metodit alkaa
+	// Haetaan kaikki kyselyt REST-metodi
+	@GetMapping("/surveys")
+	public @ResponseBody List<Survey> surveyListRest() {
+		return (List<Survey>) sRepo.findAll();
+	}
+
+	// Haetaan yhden kyselyn kysymykset REST-metodi
+	@GetMapping("/surveys/{surveyId}")
+	public @ResponseBody Optional<Survey> questionListRest(@PathVariable("surveyId") Long surveyId) {
+		return sRepo.findById(surveyId);
+	}
+
+	// Haetaan kaikki vastaukset REST-metodi
+	@GetMapping("/answers")
+	public @ResponseBody List<Answer> answersListRest() {
+		return (List<Answer>) aRepo.findAll();
+	}
+
+	// Tallenna yhden vastauksen
+	@PostMapping("/saveanswers")
+	public @ResponseBody void saveAnswerRest(@RequestBody List<Answer> answers, Answer answer) {
+
+		for (int i = 0; i < answers.size(); i++) {
+			answer = answers.get(i);
+			aRepo.save(answer);
+		}
+
+	}
 
 	@GetMapping(value = { "/", "/resthome" })
 	public String getHome() {
@@ -42,7 +81,6 @@ public class SurveyController {
 		model.addAttribute("questions", qRepo.findBySurvey(surveyName));
 		return "questions";
 	}
-
 
 	// Tekee tyhjän surveyn addNewSurvey() "/addsurvey"
 	@GetMapping("/addsurvey")
